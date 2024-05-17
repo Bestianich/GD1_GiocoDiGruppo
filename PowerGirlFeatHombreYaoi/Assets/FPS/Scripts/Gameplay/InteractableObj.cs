57374851458a84039c38a18602364f05a3120430
Bestianich <davidefedeli8899@gmail.com>
2024-05-17 11:24:13 +0200

    Implementato apertura porte

--- PowerGirlFeatHombreYaoi/Assets/FPS/Scripts/Gameplay/InteractableObj.cs
+++ PowerGirlFeatHombreYaoi/Assets/FPS/Scripts/Gameplay/InteractableObj.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InteractableObj : MonoBehaviour , IInteractable
 {
+    public bool door;
+    public Canvas canvas;
     public void Interact(){
         Debug.Log("Ciao");
+        if(!canvas.IsUnityNull()){
+            canvas.gameObject.SetActive(true);
+        } else if (door) {
+            gameObject.transform.Rotate(0 , 90 , 0);
+            door = false;
+        }
     }
     // Start is called before the first frame update
     void Start()
