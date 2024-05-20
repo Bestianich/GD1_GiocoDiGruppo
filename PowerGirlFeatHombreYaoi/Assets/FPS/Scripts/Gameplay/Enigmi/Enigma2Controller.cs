8cf78cb9ab09ad82cba0e15f47079088885da2a2
Best0998 <137522657+Bestianich@users.noreply.github.com>
2024-05-20 17:11:34 +0200

    Enigma 2

--- PowerGirlFeatHombreYaoi/Assets/FPS/Scripts/Gameplay/Enigmi/Enigma2Controller.cs
+++ PowerGirlFeatHombreYaoi/Assets/FPS/Scripts/Gameplay/Enigmi/Enigma2Controller.cs
@@ -12,27 +12,61 @@ using Image = UnityEngine.UI.Image;
 public class Enigma2Controller : MonoBehaviour, IPointerClickHandler
 {    
     public Material glowEffect;
-    public List<int> sequenzaSoluzione;
-
-    void Awake(){
-        sequenzaSoluzione = new List<int>(6);
-    }
+    public List<GameObject> sequenzaSoluzione;
+    public List<GameObject> spritesCliccati;
+    
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {                
+        spritesCliccati = new List<GameObject>();
+        //Debug.Log(gameObject.name);        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
+    }    
+
     public void OnPointerClick(PointerEventData eventData)
     {
         GameObject gameObject = eventData.pointerCurrentRaycast.gameObject;
         Debug.Log("Clicked: " + gameObject.name);        
         gameObject.GetComponent<Image>().color = Color.red;
-        //eventData.pointerCurrentRaycast.gameObject.
+        if(spritesCliccati.Count == 0){
+            spritesCliccati.Add(gameObject);
+        } else if (spritesCliccati.Count > 0){
+            for(int i = 0; i < spritesCliccati.Count; i++){
+                if(spritesCliccati[i].Equals(gameObject)){
+                    return;
+                }                
+            }
+            Debug.Log(gameObject.name);
+            spritesCliccati.Add(gameObject);
+        }        
+    }
+
+    public void button(){
+        if(ControlloSoluzione()){
+            Debug.Log("Bravo");
+            UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+            UnityEngine.Cursor.visible = false;
+            gameObject.SetActive(false);    
+        } else {
+            Debug.Log("Scemo");
+            for(int i = 0; i < spritesCliccati.Count; i++){                
+                spritesCliccati[i].GetComponent<Image>().color = Color.white;
+            }
+            spritesCliccati = new List<GameObject>();
+        }
+    }
+
+    public bool ControlloSoluzione(){
+        for(int i = 0; i < spritesCliccati.Count; i++){
+            if(!spritesCliccati[i].Equals(sequenzaSoluzione[i])){
+                return false;
+            }
+        }
+        return true;
     }
 }
