871fd9d33201bbc5e1ce7dc169b595333d1d9c2f
NoemiSossella <noemi.sossella@gmail.com>
2024-06-02 19:39:18 +0200

    tatakae

--- PowerGirlFeatHombreYaoi/Assets/FPS/Scripts/Gameplay/Enigmi/Enigma6Controller.cs
+++ PowerGirlFeatHombreYaoi/Assets/FPS/Scripts/Gameplay/Enigmi/Enigma6Controller.cs
@@ -1,18 +1,23 @@
-using System.Collections;
+/*
+ Componente da dare all'enigma per popolare le posizioni vuote
+ */
+
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enigma6Controller : MonoBehaviour
+public class EnigmaManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<GameObject> _cubes = new();
+
+    public Transform[] cubesPositions;
+
+    private int positionsIndex = 0;
+
+    public void AddCube(GameObject cube)
     {
-        
+        _cubes.Add(cube);
+        cube.transform.position = cubesPositions[positionsIndex].position;
+        positionsIndex++;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
