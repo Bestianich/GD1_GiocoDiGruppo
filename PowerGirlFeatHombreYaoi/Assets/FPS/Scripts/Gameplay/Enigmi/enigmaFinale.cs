a2e2a36e726fd144ea2ae3f6417c49ce894c64eb
NoemiSossella <noemi.sossella@gmail.com>
2024-06-09 18:35:10 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- PowerGirlFeatHombreYaoi/Assets/FPS/Scripts/Gameplay/Enigmi/enigmaFinale.cs
+++ PowerGirlFeatHombreYaoi/Assets/FPS/Scripts/Gameplay/Enigmi/enigmaFinale.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
-
-public class enigmaFinale : MonoBehaviour
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine.UI;
+using UnityEditor.UI;
+using Unity.VisualScripting;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+public class enigmaFinale : MonoBehaviour, IPointerClickHandler, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    public List<GameObject> soluzione;
+    public List<GameObject> posizioni;
+    public Vector3 worldPosition;
+    public Camera cameraZoom;
+    public GameObject Lastra;
+    private Vector3 originalPos;
+    public Timer timer;
+    public GameObject button;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +29,50 @@ public class enigmaFinale : MonoBehaviour
     {
         
     }
+    public void OnPointerClick(PointerEventData eventData)
+    {        
+        Debug.Log(eventData);
+        GameObject obj = eventData.pointerCurrentRaycast.gameObject;
+        if(obj.Equals(button)){
+            OnButtonPress();
+        }
+        Debug.Log("Clicked: " + gameObject.name);        
+        //gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+
+    }    
+
+    public void OnButtonPress(){
+        Debug.Log("SMigler");
+        for(int i = 0; i < soluzione.Count; i++){            
+            if(!soluzione[i].Equals(posizioni[i].transform.GetChild(0).gameObject)){
+                timer.TogliTempo();
+                return;
+            }
+        }
+        Lastra.SetActive(true);
+        Debug.Log("Hai vinto!!");
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+       /* var mousePos = Input.mousePosition;
+        Ray ray = cameraZoom.ScreenPointToRay(mousePos);
+        if(Physics.Raycast(ray, out var hitInfo)){
+            worldPosition = hitInfo.point;
+
+        }
+        transform.position = new Vector3(worldPosition.x , originalPos.y , worldPosition.z);
+     */
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //transform.position = originalPos;
+    }
+
 }
